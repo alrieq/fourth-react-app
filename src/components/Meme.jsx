fabeb39ea20d2ddc20f8c109ecf335901a2ef4d0
alrieq <alrieq@live.com>
2024-07-17 13:54:46 +0300

    add files

---
+++ src/components/Meme.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import memesData from "../memesData";
+import html2canvas from "html2canvas";
+
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }));
+    }
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }));
+    }
+
+    function downloadMeme() {
+        const memeElement = document.querySelector(".meme");
+        html2canvas(memeElement, { useCORS: true }).then(canvas => {
+            const link = document.createElement("a");
+            link.download = "meme.png";
+            link.href = canvas.toDataURL("image/png");
+            link.click();
+        });
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+                <button
+                    className="form--button"
+                    onClick={downloadMeme}
+                >
+                    Download Meme
+                </button>
+            </div>
+            <div className="meme" id="meme">
+                <img src={meme.randomImage} className="meme--image" alt="Meme" crossOrigin="anonymous" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    );
+}
